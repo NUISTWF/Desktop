f9c55072313e3b5178538d83da826ada35fa50bb
WF <133131708+NUISTWF@users.noreply.github.com>
2023-07-26 19:54:53 +0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -3,3 +3,4 @@ print('U->untracked 未追踪状态, A->added 增加到暂存区仓库里, M->Mo
 print('git config --global http.sslVerify "false"')
 print('每次提交信息都会产生一个版本号, 版本号此时包含了上次操作parent的版本号以及tree当前目录各个文件最新的版本号')
 print('分支并没有产生备份的仓库, 只是在当前提交的文件信息中指向不同的文件进行操作')
+print('clone到本地是克隆的在线仓库最后一次修改的所有信息，main里面指向的版本信息就是最后一次修改的版本信息')
