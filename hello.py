fe26117b44369a9be2ec28824d3cf3b48cc627c7
WF <133131708+NUISTWF@users.noreply.github.com>
2023-07-26 21:48:42 +0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-print('Hello NUISTWF GitHub!')
+print('Hello In NUISTWF GitHub!')
 print('U->untracked 未追踪状态, A->added 增加到暂存区仓库里, M->Modifed 被修改状态')
 print('git config --global http.sslVerify "false"')
 print('每次提交信息都会产生一个版本号, 版本号此时包含了上次操作parent的版本号以及tree当前目录各个文件最新的版本号')
